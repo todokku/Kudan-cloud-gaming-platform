90680239b1a40d1bb2d98d15ed5e10010f64d795
张若舟 <zhangruozhou.tim98@bytedance.com>
2020-01-20 11:30:26 +0800

    upgrade ui

--- aws_cloudgaming_test.py
+++ aws_cloudgaming_test.py
@@ -1,131 +1,55 @@
-# import boto3
+import boto3
+import botocore
+import paramiko
+import os
+import threading
+import subprocess
+import time
 
-# ec2 = boto3.client('ec2', region_name='ap-southeast-1',
-#     aws_access_key_id='AKIA3BIB67JYB45OCKCY',
-#     aws_secret_access_key='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')
-# response = ec2.describe_instances()
-# print(response)
+def runRDP(func):
+    a = subprocess.getstatusoutput('python2 rdp.py')
 
+def runGame(func):
+    os.system("./GAbin/ga-client GAbin/config/client.rel.conf rtsp://18.163.151.101:8554/desktop")
 
-# import botocore
-# import paramiko
+instance_id = 'i-0aba734d414f3aefa'
+ec2 = boto3.client('ec2', region_name='ap-east-1', aws_access_key_id='AKIAJCOQ4OUTBMDONM2Q', aws_secret_access_key='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')
 
-# action = 'OFF'
-# instance_id = 'i-066050703e6640286'
+# Do a dryrun first to verify permissions
+try:
+    ec2.start_instances(InstanceIds=[instance_id], DryRun=True)
+except:
+    print("")
+try:
+    response = ec2.start_instances(InstanceIds=[instance_id], DryRun=False)
+    print(response)
+except:
+    print("")
 
-# ec2 = boto3.client('ec2', region_name='ap-southeast-1',
-#     aws_access_key_id='AKIA3BIB67JYB45OCKCY',
-#     aws_secret_access_key='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')
+time.sleep(25)
 
+ssm_client = boto3.client('ssm', region_name='ap-east-1',
+    aws_access_key_id='AKIAJCOQ4OUTBMDONM2Q',
+    aws_secret_access_key='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')
 
-# if action == 'ON':
-#     # Do a dryrun first to verify permissions
-#     try:
-#         ec2.start_instances(InstanceIds=[instance_id], DryRun=True)
-#     except ClientError as e:
-#         if 'DryRunOperation' not in str(e):
-#             raise
+threads = []
+t1 = threading.Thread(target=runRDP,args=(1,))
+threads.append(t1)
+t1.setDaemon(True)
+t1.start()
 
-#     # Dry run succeeded, run start_instances without dryrun
-#     try:
-#         response = ec2.start_instances(InstanceIds=[instance_id], DryRun=False)
-#         print(response)
-#     except ClientError as e:
-#         print(e)
-# else:
-#     # Do a dryrun first to verify permissions
-#     try:
-#         ec2.stop_instances(InstanceIds=[instance_id], DryRun=True)
-#     except ClientError as e:
-#         if 'DryRunOperation' not in str(e):
-#             raise
+time.sleep(5)
 
-#     # Dry run succeeded, call stop_instances without dryrun
-#     try:
-#         response = ec2.stop_instances(InstanceIds=[instance_id], DryRun=False)
-#         print(response)
-#     except ClientError as e:
-#         print(e)
+params2 = {"commands":["cd ../../Users/Administrator/gaminganywhere-0.8.0/bin","tasklist",r"PsExec.exe -s -i 2 'C:\Users\Administrator\gaminganywhere-0.8.0\bin\ga-server-event-driven.exe' 'C:\Users\Administrator\gaminganywhere-0.8.0\bin\config\server.assaultcube.win32.conf'"],"executionTimeout":["3600"]}
+response2 = ssm_client.send_command(DocumentName="AWS-RunPowerShellScript", InstanceIds=[instance_id],Comment='Execute Limbo', TimeoutSeconds=600, Parameters=params2)
+print(response2)
 
-# client = paramiko.SSHClient()
-# client.load_system_host_keys()
+time.sleep(10)
 
-# Connect/ssh to an instance
-# try:
-#     # Here 'ubuntu' is user name and 'instance_ip' is public IP of EC2
-#     print('no')
-#     client.connect(hostname='ec2-18-163-151-170.ap-east-1.compute.amazonaws.com', port=3389, username='Administrator', password='DrScd83t6aHUpd)feDD3KT)TbgLT%ifm')
-#     print('ok')
-#     # # Execute a command(cmd) after connecting/ssh to an instance
-#     stdin, stdout, stderr = client.exec_command('dir')
-#     print(stdin.read())
-#     print(stderr.read())
-#     print(stdout.read())
+t2 = threading.Thread(target=runGame,args=(1,))
+threads.append(t2)
+t2.setDaemon(True)
+t2.start()
 
-#     # # close the client connection once the job is done
-#     client.close()
-
-# except Exception as e:
-#     print (e)
-
-# import spur
-# shell = spur.SshShell(hostname='ec2-18-163-151-170.ap-east-1.compute.amazonaws.com', port=3389, username="Administrator", password="DrScd83t6aHUpd)feDD3KT)TbgLT%ifm")
-# result = shell.run(["echo", "-n", "hello"])
-# print(result.output) # prints hello
-
-# import winrm
-
-# wintest = winrm.Session('http://18.163.152.147:5985/wsman',auth=('Administrator','A(nI9;qULc;KKA@HJAtUp)5$dE8XntvG'), read_timeout_sec=60)
-# ret = wintest.run_cmd('ipconfig')
-# print(ret)
-from rdpy.protocol.rdp import rdp
-class MyRDPFactory(rdp.ClientFactory):
-
-    def clientConnectionLost(self, connector, reason):
-        reactor.stop()
-
-    def clientConnectionFailed(self, connector, reason):
-        reactor.stop()
-
-    def buildObserver(self, controller, addr):
-
-        class MyObserver(rdp.RDPClientObserver):
-
-            def onReady(self):
-                """
-                @summary: Call when stack is ready
-                """
-                #send 'r' key
-                self._controller.sendKeyEventUnicode(ord(unicode("r".toUtf8(), encoding="UTF-8")), True)
-                #mouse move and click at pixel 200x200
-                self._controller.sendPointerEvent(200, 200, 1, true)
-
-            def onUpdate(self, destLeft, destTop, destRight, destBottom, width, height, bitsPerPixel, isCompress, data):
-                """
-                @summary: Notify bitmap update
-                @param destLeft: xmin position
-                @param destTop: ymin position
-                @param destRight: xmax position because RDP can send bitmap with padding
-                @param destBottom: ymax position because RDP can send bitmap with padding
-                @param width: width of bitmap
-                @param height: height of bitmap
-                @param bitsPerPixel: number of bit per pixel
-                @param isCompress: use RLE compression
-                @param data: bitmap data
-                """
-                
-            # def onSessionReady(self):
-		        # """
-		        # @summary: Windows session is ready
-		        # """
-
-            # def onClose(self):
-                # """
-                # @summary: Call when stack is close
-                # """
-
-        return MyObserver(controller)
-
-from twisted.internet import reactor
-reactor.connectTCP("18.163.151.101", 3389, MyRDPFactory())
-reactor.run()
+while True:
+    time.sleep(10)
--- main.py
+++ main.py
@@ -1,27 +1,74 @@
 # -*- coding: UTF-8 -*-
 import wx
 import boto
+# from init_game import CloudGame
+
 
 def OnQuit(e):
     main_frame.Close()
 
-def ConnectToEC2(e):
-    ec2 = boto3.client('ec2', region_name='ap-east-1b',
-    aws_access_key_id='AKIAJCOQ4OUTBMDONM2Q',
-    aws_secret_access_key='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')
-    response = ec2.describe_instances()
-    print(response)
+def ConnectToGame(e):
+    return 0
+    # game = CloudGame()
+    # game.startInstance()
+    # game.startGame()
+    
+def ChangeGameSelection(e):
+    section = game_list_box.GetSelections()[0]
+    game_name = game_list_box.GetString(section)
+    game_name_label.SetLabel('Game Name: '+game_name)
+    game_genre_label.SetLabel('Game Genre: '+game_dict[game_name][0])
+    game_area_label.SetLabel('Graphic Engine: '+game_dict[game_name][1])
+    start_image = wx.Image(game_dict[game_name][2])
+    start_image.Rescale(200, 100)
+    game_logo_widget.SetBitmap(wx.BitmapFromImage(start_image))
     
 app = wx.App(False)
 main_frame = wx.Frame(None, 0, "Kudan Cloud Gaming Platform", size=(600, 600), pos=(20, 20))
 main_frame.Show(True)
 
-connect_button = wx.Button(main_frame, label='CONNECT', size=(100, 70), pos=(250, 300))
-connect_button.Bind(wx.EVT_BUTTON, ConnectToEC2)
+connect_button = wx.Button(main_frame, label='CONNECT', pos=(250, 500))
+connect_button.SetBackgroundColour(wx.Colour(64, 64, 64))
+connect_button.Bind(wx.EVT_BUTTON, ConnectToGame)
 
-quit_button = wx.Button(main_frame, label='QUIT', size=(96, 96), pos=(0, 0))
-quit_button.SetBitmap(wx.Bitmap("quit.png"),wx.LEFT)
+quit_button = wx.Button(main_frame, label='QUIT', pos=(0, 0))
+quit_button.SetBackgroundColour(wx.Colour(64, 64, 64))
 quit_button.Bind(wx.EVT_BUTTON, OnQuit)
+
+# 服务器列表栏
+server_list = ['i-0aba734d414f3aefa']
+server_list_box = wx.ListBox(main_frame, -1, (100, 70), (200, 150), server_list, wx.LB_SINGLE)
+server_list_box.SetSelection(0)
+main_frame.Bind(wx.EVT_LISTBOX, ChangeGameSelection, server_list_box)
+
+# 服务器信息栏
+server_dict = {'i-0aba734d414f3aefa':['g4dn.xlarge','ap-east-1b', '18.163.151.101']}
+instance_data1 = server_dict[server_list[0]]
+server_id_label = wx.StaticText(parent=main_frame, label='Instance ID: '+server_list[0], pos=(350, 70))
+server_type_label = wx.StaticText(parent=main_frame, label='Instance Type: '+instance_data1[0], pos=(350, 100))
+server_area_label = wx.StaticText(parent=main_frame, label='Instance Area: '+instance_data1[1], pos=(350, 130))
+server_ip_lable = wx.StaticText(parent=main_frame, label='Instance IP: '+instance_data1[2], pos=(350, 160))
+
+# 游戏列表栏
+game_list = ['AssaultCube', 'LIMBO', 'Cube2: Sauerbraten']
+game_list_box = wx.ListBox(main_frame, -1, (100, 250), (200, 200), game_list, wx.LB_SINGLE)
+game_list_box.SetSelection(0)
+main_frame.Bind(wx.EVT_LISTBOX, ChangeGameSelection, game_list_box)
+
+game_dict = {'AssaultCube':['FPS', 'SDL', 'gameLogo/AssaultCube.png'], 
+             'LIMBO':['ACT', 'DirectX9', 'gameLogo/LIMBO.png'], 
+             'Cube2: Sauerbraten':['FPS', 'SDL', 'gameLogo/Cube2: Sauerbraten.png']}
+
+# 游戏信息栏
+game_data1 = game_dict[game_list[0]]
+game_name_label = wx.StaticText(parent=main_frame, label='Game Name: '+game_list[0], pos=(350, 250))
+game_genre_label = wx.StaticText(parent=main_frame, label='Game Genre: '+game_data1[0], pos=(350, 280))
+game_area_label = wx.StaticText(parent=main_frame, label='Graphic Engine: '+game_data1[1], pos=(350, 310))
+start_image = wx.Image(game_data1[2])
+start_image.Rescale(200, 100)
+bitmap = wx.BitmapFromImage(start_image)
+game_logo_widget = wx.StaticBitmap(main_frame, -1, bitmap)
+game_logo_widget.SetPosition((350, 340))
 
 app.MainLoop()
 
